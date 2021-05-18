bb29f44665fd58c546cda315687d7bc69dc58e9c
Darbaz Ali <darbaz.me@gmail.com>
2021-05-18 16:56:02 +0300

    say Hi to interfaces

--- app.ts
+++ app.ts
@@ -5,3 +5,27 @@ const element  = document.createElement('h2')
 element.textContent = message
 
 document.body.appendChild(element)
+
+
+// creating an interface
+interface Product {
+    id: number,
+    name: string,
+    price: number
+}
+
+function getProduct(id): Product {
+    return {
+        id: id,
+        name: `Awesome Gadget ${id}`,
+        price: 99.5
+    }
+}
+
+const product = getProduct(1)
+
+const showProduct = (name: string, price: number) => {
+    console.log(`The product ${name} costs $${price}`)
+}
+
+showProduct(product.name, product.price)
